be286f162289660818b70051d82943ead5e1532e
Yk <slavchyk21122005@gmail.com>
2022-08-27 11:39:13 +0300

    gitignore

---
+++ OOP/OOP/OOP.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+// OOP - Object Oriented programming(Abstract)
+// POP - Problem Oriented programming(Procedure) - JS(React,Node,Typescript)
+
+// Abstract - orginized code
+// Incapsulation (can't change what already was there, but can add new things)
+// Polymorphysm
+// Inheritence
+
+// LCD matrix
+// Case
+// LED ligh
+// Buttons
+// Power
+
+// class = API - Abstract
+// private, public, protected, internal -  Incapsulation
+// override, overload - Polymorphysm
+
+
+/*
+ Abstract
+int sum(int a, int b) 
+{
+    return 0;
+}
+
+int mult(int a, int b)
+{
+    return 0;
+}
+
+
+-Trash code
+void OpenBrowser(string browserName)
+{
+    cout << browserName << "\n";
+}
+-Trash code
+*/
+
+/* -- incapsulation
+
+
+
+const string key = "123";
+
+void connectToDb()
+{
+    cout << key;
+}
+
+void connectToDb(string newKey)
+{
+    cout << newKey;
+}
+
+void login()
+{
+    connectToDb();
+    cout << "Login";
+}
+
+int main()
+{
+    connectToDb();
+    connectToDb("fdfdf");
+
+
+}
+*/
+
+/*
+// Polymorphysm
+// ExtreemeCode - YT 
+class Cat
+{
+
+};
+
+class Dog
+{
+
+};
+
+class Veterinarian
+{
+    void Visit(Cat cat)
+    {
+        cout << "1";
+    }
+
+    void Visit(Dog dog)
+    {
+        cout << "2";
+    }
+
+};
+*/
+
+/*
+// Inheritance
+class Animal{
+    void Step()
+    {
+
+    }
+
+    void Eat()
+    {
+
+    }
+};
+class Cat : Animal
+{
+
+
+};
+
+class Dog : Animal
+{
+
+};
+
+class Veterinarian
+{
+    void Visit(Cat cat)
+    {
+        cout << "1";
+    }
+
+    void Visit(Dog dog)
+    {
+        cout << "2";
+    }
+
+    void Visit(Animal animal)
+    {
+        
+    }
+
+};
+*/
+
+// GIT Version Control System (VCS)
