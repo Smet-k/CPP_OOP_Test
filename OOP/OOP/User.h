5b7a468dac42c99edcd065ca4a178fda1baa7387
Yk <slavchyk21122005@gmail.com>
2022-08-31 15:42:51 +0300

    age counter

--- OOP/OOP/User.h
+++ OOP/OOP/User.h
@@ -8,10 +8,20 @@ class User
 public:
 	void reg()
 	{
+		int bd, bm, by;
 		string tmp;
-		cout << "Enter your birthday(yr/mm/dd): ";
-		cin >> birthday;
-		birthdayCheck(birthday);
+		while (true) {
+			cout << "Enter your birthday in format dd mm yyyy: \n";
+			cin >> bd >> bm >> by;
+			if(countAge(bd, bm, by) == true)
+			{
+				break;
+			}
+			else
+			{
+				cout << "You should be 14 or older to register.\n";
+			}
+		}
 		cout << "Enter your name: ";
 		cin >> name;
 		cout << "Enter your surname: ";
@@ -28,33 +38,43 @@ public:
 
 
 private:
+	int cd = 31;
+	int cm = 8;
+	int cy = 2022;
 	string username;
 	string name;
 	string surname;
-	string birthday;
+	string bd,bm,by;
 	string country;
 	string password;
 
-	bool birthdayCheck(string input)
+	bool countAge(int bd, int bm, int by)
 	{
-		string word = "";
-		vector<string> date;
-		
-
-		for(int j = 0;j < input.size();j++)
+		int  d, m, y;
+		int md[12] = { 31,28,31,30,31,30,31,31,30,31,30,31};
+		y = cy - by;
+		if (cm < bm)
 		{
-			if(input[j] != '/')
-			{
-				word += input[j];
-			}
-			else
-			{
-				date.push_back(word);
-				word = "";
-			}
+			y--;
+			m = 12 - (bm - cm);
 		}
-		return false;
+		else
+		{
+			m = cm - bm;
+		}
+		if(cd < bd)
+		{
+			m--;
+			d = md[cm - 1] - (bd - cd);
+		}
+		else
+		{
+			d = cd - bd;
+		}
+		if (y < 14) { return false; }
+		return true;
 	}
+
 
 	void setPassword(string value)
 	{
